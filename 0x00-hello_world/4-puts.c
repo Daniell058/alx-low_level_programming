dddcba63cffb7f1f47225fdf9d0a4177f2965ee5
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-13 22:27:39 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ *main - Entry Poiny
+ *Description: Prints "Programming is like building a multilingual puzzle
+ *Return: always 0
+*/
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
