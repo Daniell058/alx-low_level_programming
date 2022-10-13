0d2ae3796be7466eaba655c9a79b8be38af43672
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-13 22:57:58 +0100

    Added 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+printf("size of a char: %lu\n", sizeof(char));
+printf("size of a int: %lu\n", sizeof(int));
+printf("size of a long int: %lu\n", sizeof(long int));
+printf("size of a long long int: %lu\n", sizeof(long long int));
+printf("size of a float: %lu\n", sizeof(float));
+return (0);
+}
