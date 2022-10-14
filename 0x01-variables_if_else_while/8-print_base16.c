34bd3c7bfd638d80c691770436be2897da0a71da
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-15 00:10:58 +0100

    Added 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description: prints something fun
+ *Return: always 0
+*/
+
+int main(void)
+{
+char letter;
+int num;
+
+for (num = '1'; num <= '9'; num++)
+{
+putchar(num);
+}
+
+for (letter = 'a'; letter <= 'f'; letter++)
+{
+putchar(letter);
+}
+
+putchar('\n');
+return (0);
+}
