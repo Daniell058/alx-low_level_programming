c0689c5901a1b2aba6bbc801cc3f920934d6a58a
Daniel <100762022+Daniell058@users.noreply.github.com>
2022-10-15 11:27:27 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,8 +22,11 @@ if (!(num1 == num2) && !(num2 == num3) && !(num1 == num3))
 putchar(num1);
 putchar(num2);
 putchar(num3);
+if (!((num1 == '7') && (num2 == '8') && (num3 == '9')))
+{
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
