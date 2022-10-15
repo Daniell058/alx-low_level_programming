947df88ef15f8c22de9b1a7b4a48a2f0c265692a
Daniel <100762022+Daniell058@users.noreply.github.com>
2022-10-15 10:47:07 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,16 +11,17 @@ int main(void)
 {
 int num1;
 int num2;
-
-for (num1 = '0'; num1 <= '8'; num1++)
+for (num1 = '0'; num1 <= '9'; num1++)
 {
-for (num2 = '1'; num2 <= '9'; num2++)
+for (num2 = num1; num2 <= '9'; num2++)
 {
-
 putchar(num1);
 putchar(num2);
-putchar(',');
-putchar(' ');
+if (num1 < '9')
+{
+    putchar(',');
+    putchar(' ');
+}
 }
 }
 putchar('\n');
