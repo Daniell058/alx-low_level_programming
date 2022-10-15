5cc3f69a66e4cd232d9e036221d517b29305d2fe
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-15 09:49:09 +0100

    add 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+/**
+ *main - Entry point
+ *Description: prints something fun
+ *Return: always 0
+*/
+
+int main(void)
+{
+int num1;
+int num2;
+
+for (num1 = '0'; num1 <= '8'; num1++)
+{
+for (num2 = '1'; num2 <= '9'; num2++)
+{
+
+putchar(num1);
+putchar(num2);
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+}
