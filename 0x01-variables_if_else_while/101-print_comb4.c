372b6501d820d1b22e55b9df13e3f9764dd16830
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-15 11:20:45 +0100

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  *main - Entry point
  *Description: prints something fun
@@ -11,17 +10,18 @@ int main(void)
 {
 int num1;
 int num2;
+int num3;
 for (num1 = '0'; num1 <= '9'; num1++)
 {
 for (num2 = num1; num2 <= '9'; num2++)
 {
-if (!(num1 == num2))
+for (num3 = num2; num3 <= '9'; num3++)
+{
+if (!(num1 == num2) && !(num2 == num3) && !(num1 == num3))
 {
 putchar(num1);
 putchar(num2);
-
-if (num1 < '8')
-{
+putchar(num3);
 putchar(',');
 putchar(' ');
 }
