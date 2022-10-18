b4acbf62ec5fd37ebcf43a9efdc2c035e30fb408
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-18 23:45:15 +0100

    Added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - returns the absolute value of a number
+ *@x: must be of type integer
+ *Return: The absolute value of the number
+*/
+
+int _abs(int x)
+{
+	int num = abs(x);
+
+	return (num);
+}
