399391aec2a84f055fb5e82fa2b3820321f6749a
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-18 23:14:42 +0100

    Added 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+
+/**
+* _isalpha - checks whether an input is alphabetic
+*@c: any given input
+*Return: Either 0 or 1
+*/
+
+int _isalpha(int c)
+{
+	if isalpha(c)
+		return(1)
+	else
+		return(0)
+}
