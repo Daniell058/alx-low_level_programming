9c225fcb77bb6eff3e61aae73943ba66b05bd564
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-18 23:26:55 +0100

    Added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+/**
+ * print_sign - prints the sign of a number
+ *@n: Must be of type int
+ *Return: Depends on the sign
+*/
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+
+	if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+
+	if (n < 0)
+	{
+		_putchar ('-');
+		return (-1);
+	}
+	return (0)
+}
