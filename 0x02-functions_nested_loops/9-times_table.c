c98feef87a633e7b6c134baf7e65a784d23f76c6
Daniel <100762022+Daniell058@users.noreply.github.com>
2022-10-19 02:30:26 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,7 +17,7 @@ void times_table(void)
         {
             int times = (i * j);
 
-            printf("%2d", times);
+            printf("%2d ", times);
             if (j < 9)
                 printf(",");
         }
