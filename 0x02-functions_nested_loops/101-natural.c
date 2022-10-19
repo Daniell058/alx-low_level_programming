c5fa167313996204ac6e0396e9fa4a02e18c9747
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-19 15:15:17 +0100

    Added  101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *multiples_of_three - get the sum of all multiples of 3 and 5 below 1024
+ *
+ *Return: Always void
+*/
+
+void multiples_of_three(void)
+{
+	int i;
+	int sum;
+
+	sum = 0;
+	for (i = 0; i < 1023; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d", sum);
+}
