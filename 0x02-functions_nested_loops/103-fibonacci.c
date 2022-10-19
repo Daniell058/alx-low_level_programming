444b97a9bfc39542cc4a19ecbccea53272fb441a
Daniel <100762022+Daniell058@users.noreply.github.com>
2022-10-19 16:23:20 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,9 +9,9 @@
 
 int main(void)
 {
-	long long num1, num2, num3;
+	long num1, num2, num3;
 	int i;
-	long long sum;
+	llong sum;
 
 	num1 = 1;
 	num2 = 2;
@@ -23,6 +23,6 @@ int main(void)
 		if ((num1 < 4000000) && (num1 % 2 == 0))
 			sum += num1;
 	}
-	printf("%lli\n", sum);
+	printf("%li\n", sum);
 	return (0);
 }
