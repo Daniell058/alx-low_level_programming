16ddb281846946bce4d287be4da7dce575216e57
Daniel <100762022+Daniell058@users.noreply.github.com>
2022-10-19 02:07:53 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,35 +8,35 @@
 */
 
 void times_table(void)
-{	int i, j;
+{
 
-	 int i, j;
+	int i, j;
     int first, last;
     for (i = 0; i <= 9; i++)
     {
-        for (j = 0; j <= 9; j++)
-        {
-            int times = (i * j);
-            first = times;
-            last = (times % 10);
-            while (first >= 10)
-            {
-                first = first / 10;
-            }
-            if (times > 10)
-            {
-                _putchar('0' + first);
-                _putchar('0' + last);
-            }
-            else
-                _putchar('0' + first);
+	for (j = 0; j <= 9; j++)
+	{
+	    int times = (i * j);
+	    first = times;
+	    last = (times % 10);
+	    while (first >= 10)
+	    {
+		first = first / 10;
+	    }
+	    if (times > 10)
+	    {
+		_putchar('0' + first);
+		_putchar('0' + last);
+	    }
+	    else
+		_putchar('0' + first);
 
-            if (j < 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+	    if (j < 9)
+	    {
+		_putchar(',');
+		_putchar(' ');
+	    }
+	}
+	_putchar('\n');
+    
 }
