801e5b112a475a3b54d91c37a82145df9240cae8
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-19 23:38:28 +0100

    Alx Debugging Tasks

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - Tests the function positive_or_negative
+ *
+ *Return: Always 0
+*/
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+	positive_or_negative(0);
+	return (0);
+}
