65ae5fa2aae50123fe0d666cd40a6a0a2f380bb3
Daniel <100762022+Daniell058@users.noreply.github.com>
2022-10-19 15:20:39 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,7 +3,7 @@
 
 /**
  *multiples_of_three - get the sum of all multiples of 3 and 5 below 1024
- *
+ *main - prints multiple_of_three
  *Return: Always void
 */
 
@@ -13,7 +13,7 @@ void multiples_of_three(void)
 	int sum;
 
 	sum = 0;
-	for (i = 0; i < 1023; i++)
+	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
