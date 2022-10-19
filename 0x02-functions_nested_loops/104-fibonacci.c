e63785fd71257d2f01289b87721b22991fc917bb
Daniel <100762022+Daniell058@users.noreply.github.com>
2022-10-19 17:23:36 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,13 +13,13 @@ int main(void)
 
 	num1 = 1;
 	num2 = 2;
-	printf("%lu", num1);
+	printf("%ld", num1);
 	for (i = 2; i <= 98; i++)
 	{
 		num3 = num1 + num2;
 		num1 = num2;
 		num2 = num3;
-		printf(", %lu", num1);
+		printf(", %ld", num1);
 	}
 	printf("\n");
 	return (0);
