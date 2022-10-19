e19421b2f6f8de50060546d57c7dff6f22bd7f48
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-19 02:40:16 +0100

    added 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *add - adds two numbers
+ *@x: must be of type int
+ *@y: must be of type int
+ *Return: the sum
+*/
+
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,18 +9,18 @@
 
 void times_table(void)
 {
-   int i, j;
+	int i, j;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            int times = (i * j);
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int times = (i * j);
 
-            printf("%2d", times);
-            if (j < 9)
-                printf(", ");
-        }
-            printf("\n");
-    }
+			printf("%2d", times);
+			if (j < 9)
+				printf(", ");
+		}
+		printf("\n");
+	}
 }
