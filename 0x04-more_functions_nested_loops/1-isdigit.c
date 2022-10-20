7b9977103a444c76136ad6900382a532dee856c9
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-20 11:56:54 +0100

    added  1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *_isdigit - checks if a number is a digit
+ *
+ *@c:parameter
+ *
+ *Return: Either 0 or 1
+*/
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+		return (1);
+	else
+		return (0)
+}
+
