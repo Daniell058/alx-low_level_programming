21e605930d90a401e78942359a5ca36b278875ea
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-20 13:43:11 +0100

    added 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *more_numbers - prints numbers one to 14 10 times
+ *
+ *Return: Always void
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('0' + (j / 10));
+
+			_putchar('0' + (j % 10));
+		}
+		_putchar('\n');
+	}
+}
