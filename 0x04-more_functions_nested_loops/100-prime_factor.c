ab48582db8ab18bd093a50ac437e6b78e33f2c3d
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-20 19:01:10 +0100

    Daniel

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: always 0
+*/
+
+int main(void)
+{
+	int i, j, prod;
+
+	unsigned long n = 612852475143;
+
+	prod = 1;
+
+	for (i = 1; i <= 99, i++)
+	{
+		while (n % i == 0)
+		{
+			prod *= i;
+			n = n / i;
+		}
+	}
+	printf("%d", prod);
+	return (0);
+}
+
