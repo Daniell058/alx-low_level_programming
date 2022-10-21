197a0b9c451d11f155160b6002729e48013f8ecb
Daniel <100762022+Daniell058@users.noreply.github.com>
2022-10-21 08:33:11 +0100

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,21 +7,24 @@
 
 int main(void)
 {
-	int i, prod;
+    int i;
+    unsigned long prod, pass;
 
-	unsigned long n = 612852475143;
+    unsigned long n = 612852475143;
 
-	prod = 1;
+    pass = n;
 
-	for (i = 2; i <= 99; i++)
-	{
-		while (n % i == 0)
-		{
-			prod *= i;
-			n = n / i;
-		}
-	}
-	printf("%d\n", prod);
-	return (0);
+    prod = 1;
+
+    for (i = 2; i <= 99; i++)
+    {
+        while (n % i == 0)
+        {
+            prod *= i;
+            n = n / i;
+        }
+    }
+    printf("%li\n", (pass / prod));
+    return (0);
 }
 
