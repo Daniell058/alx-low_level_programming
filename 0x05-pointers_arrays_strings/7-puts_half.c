22d543363903064e79556eaf832a5179e6932482
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-24 20:52:23 +0100

    added 8-print_array.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,35 +3,35 @@
 #include <string.h>
 
 /**
- *puts_half: prints half of a string
+ *puts_half - prints half of a string
  *@str: a string
  *Return: nothing
 */
 
 void puts_half(char *str)
 {
-    int len = (strlen(str));
-    int i = 0;
+	int len = (strlen(str));
+	int i = 0;
 
-    if (len % 2 != 0)
-    {
-        int n = (len - 1) / 2;
+	if (len % 2 != 0)
+	{
+		int n = (len - 1) / 2;
 
-        for (i = n + 1; i < len; i++)
-        {
-            putchar(str[i]);
-        }
-        putchar('\n');
-    }
+		for (i = n + 1; i < len; i++)
+		{
+			putchar(str[i]);
+		}
+		putchar('\n');
+	}
 
-    else
-    {
-        int n = len / 2;
+	else
+	{
+		int n = len / 2;
 
-        for (i = n; i < len; i++)
-        {
-            putchar(str[i]);
-        }
-        putchar('\n');
-    }
+		for (i = n; i < len; i++)
+		{
+			putchar(str[i]);
+		}
+		putchar('\n');
+	}
 }
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_array - printts array to a specific point
+ *@a: the array
+ *@n: where you want to print to
+ *Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < (n - 1))
+		{
+			printf(", ");
+		}
+	}
+}
