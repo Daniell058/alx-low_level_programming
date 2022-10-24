78298c12a14a5d4d1c70b65333c1c6fe66e8d4a6
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-24 20:30:55 +0100

    created 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *puts_half: prints half of a string
+ *@str: a string
+ *Return: nothing
+*/
+
+void puts_half(char *str)
+{
+    int len = (strlen(str));
+    int i = 0;
+
+    if (len % 2 != 0)
+    {
+        int n = (len - 1) / 2;
+
+        for (i = n; i <= len; i++)
+        {
+            putchar(str[i]);
+        }
+        putchar('\n');
+    }
+
+    else
+    {
+        int n = len / 2;
+
+        for (i = n; i <= len; i++)
+        {
+            putchar(str[i]);
+        }
+        putchar('\n');
+    }
+}
