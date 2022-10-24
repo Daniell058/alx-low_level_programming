aa51ce9322d580c06791ba9afc1ff757e3a0884d
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-10-24 20:17:13 +0100

    create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *puts2 - prints every other character of a string
+  *
+  *@str: param 1
+  *
+  *Return: nothing
+*/
+
+
+
+
+
+void puts2(char *str)
+{
+	int i;
+	int len = (strlen(str) - 1);
+
+	for (i = 0; i <= len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
