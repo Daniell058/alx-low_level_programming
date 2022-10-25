95b34ef9ae32d279bd4efa5784a4f454cf9d1813
Daniel <100762022+Daniell058@users.noreply.github.com>
2022-10-25 06:38:02 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,5 +10,35 @@
 
 int _atoi(char *s)
 {
-	return (atoi(s));
+    int i;
+    int num = 0;
+    int sign = 1;
+    int len = (strlen(s) - 1);
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == '-')
+        {
+            sign *= -1;
+            continue;
+        }
+
+        if (s[i] < '0' || s[i] > '9')
+        {
+            continue;
+        }
+
+        else
+        {
+            {
+                int how_big = pow(10, (len - i));
+                num += ((how_big) * (s[i] - '0'));
+
+                if (s[i + 1] < '0' || s[i + 1] > '9')
+                    break;
+            
+        }
+    }
+}
+return (num * sign);
 }
