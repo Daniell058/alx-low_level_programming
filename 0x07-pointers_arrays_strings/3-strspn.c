09d91190c8c8f591d694d1ab9a5f8bd6e3cbb5ba
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-11-01 19:36:43 +0100

    added 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *_strspn - imitates built in strspn
+ *@s: param 1
+ *@accept: param 2
+ *Return: k
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	unsigned int j;
+	unsigned int k = 0;
+
+	while (s[i])
+	{
+		while (accept[j])
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+				{
+					k++;
+					break;
+				}
+
+				if (accept[j + 1] == '\0' && s[i] != accept[j])
+				{
+					return (k);
+				}
+			}
+			i++;
+		}
+		return (k);
+	}
+}
