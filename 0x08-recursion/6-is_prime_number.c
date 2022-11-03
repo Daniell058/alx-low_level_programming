5fcea19841ce8424f63cffcad7dc3960f7e3938d
Daniel <100762022+Daniell058@users.noreply.github.com>
2022-11-03 15:24:49 +0100

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,17 +10,17 @@
 
 
 int prime_no_checker(int n, int a)
-{
+{	
+	if (!(a > (n / 2)))
+	{
+		prime_no_checker(n, a + 1);
+	}
 
 	if ((n % a) == 0)
 	{
 		return (0);
 	}
 
-	if (!(a > (n / 2)))
-	{
-		prime_no_checker(n, a + 1);
-	}
 
 	return (1);
 }
