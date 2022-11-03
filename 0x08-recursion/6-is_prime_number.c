e706e062c12343f6dc9b52bdb4f62f106ece716a
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-11-03 14:49:31 +0100

    added 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *prime_no_checker - checks if a number is a prime number
+ *@n: the number
+ *@a: num to start from
+ *Return: 1 or 0
+*/
+
+
+
+
+int prime_no_checker(int n, int a)
+{
+	if (a == n / 2)
+	{
+		return (1);
+	}
+
+	if (n % a == 0)
+	{
+		return (0);
+	}
+
+	prime_no_checker(n, a + 1);
+}
+
+/**
+ *is_prime_number - checks if a number is a prime number
+ *@n: the number
+ *Return: 1 or 0
+*/
+
+
+int is_prime_number(int n)
+{
+	if (prime_no_checker(n, 2))
+	{
+		return (1);
+	}
+	return (0);
+}
