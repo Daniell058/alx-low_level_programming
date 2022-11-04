e43dd354fc0b28907a175782db78bebe175ca77e
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-11-04 21:35:58 +0100

    added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ *main - prints the number of arguments
+ *@argc: no of arguments
+ *@argv: arguments array
+ *Return:always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
