f30a95d3f3803b86c5337524ce65395285ad4a58
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-11-04 21:08:11 +0100

    added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *main - prints the name of a c executable
+ *@argc: number of arguments given
+ *@argv: an array of argc
+ *Return: void
+*/
+
+void main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+}
