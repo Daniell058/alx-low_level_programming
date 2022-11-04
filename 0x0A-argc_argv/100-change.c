4dc38176b6824cea1e14350c5f959c78f13a44bc
Daniel Akintayo <akintayodaniel03@gmail.com>
2022-11-04 23:07:03 +0100

    added

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *main - prints the minimum number of cents
+ *@argc: no of arguments
+ *@argv: arguments array
+ *Return:always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+
+	if (atoi(argv[1]) < 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	int change[] = {25, 10, 5, 2, 1};
+	int cents = atoi(argv[1]);
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 5; i++)
+	{
+		if (cents >= change[i])
+		{
+
+			int x = cents / change[i];
+
+			sum += x;
+			cents -= (x * change[i]);
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
